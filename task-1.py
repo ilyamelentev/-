6e363146189063ed83361f48c6ae5fa6f68d05c5
ilyamelentev <gagarinila9@gmail.com>
2026-03-20 03:26:40 +0500

    Add files via upload

--- task-1.py
+++ task-1.py
@@ -1,20 +1,26 @@
-src = not False and True or False and not True
+def find_common_participants(group1, group2, delimiter=','):
 
-# Упрощение выражения по шагам:
+    list1 = group1.split(delimiter)
+    list2 = group2.split(delimiter)
 
-# 1. Упростить оператор `not`:
-#    not False → True
-#    not True → False
-#    Получаем: True and True or False and False
 
-# 2. Упростить оператор `and` (выполняется до `or`):
-#    True and True → True
-#    False and False → False
-#    Получаем: True or False
+    common = set(list1) & set(list2)
 
-# 3. Упростить оператор `or`:
-#    True or False → True
 
-result = True  
+    return sorted(list(common))
 
-print(src == result)  
+
+
+participants_first_group = "Иванов,Петров,Сидоров"
+participants_second_group = "Петров,Сидоров,Смирнов"
+
+common_participants = find_common_participants(participants_first_group, participants_second_group)
+print("Общие участники (запятая):", common_participants)
+
+
+participants_first_group_pipe = "Иванов|Петров|Сидоров"
+participants_second_group_pipe = "Петров|Сидоров|Смирнов"
+
+common_participants_pipe = find_common_participants(participants_first_group_pipe, participants_second_group_pipe,
+                                                    delimiter='|')
+print("Общие участники (вертикальная черта):", common_participants_pipe)
